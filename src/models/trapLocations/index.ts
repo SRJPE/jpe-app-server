a722a31f908762ccc79274c2c2f8ea546506df4e
Ben Pintel <Bpintel@gmail.com>
2024-09-05 09:02:47 -0700

    adds updateTrapLocations

--- src/models/trapLocations/index.ts
+++ src/models/trapLocations/index.ts
@@ -62,4 +62,47 @@ async function postTrapLocations(
   }
 }
 
-export { getProgramTrapLocations, postTrapLocations }
+async function updateTrapLocations(
+  trapLocationsValues
+): Promise<TrapLocations[]> {
+  try {
+    let trappingSitesPayload = []
+    let releaseSitesPayload = []
+    trapLocationsValues.forEach((trappingSite: any) => {
+      trappingSitesPayload.push({
+        programId: trappingSite.programId,
+        trapName: trappingSite.trapName,
+        dataRecorderId: trappingSite.dataRecorderId,
+        dataRecorderAgencyId: trappingSite.dataRecorderAgencyId,
+        siteName: trappingSite.siteName,
+        coneSizeFt: trappingSite.coneSizeFt,
+        xCoord: trappingSite.xCoord,
+        yCoord: trappingSite.yCoord,
+        gageAgency: trappingSite.gageAgency,
+        createdAt: trappingSite.createdAt,
+        updatedAt: trappingSite.updatedAt,
+      })
+      releaseSitesPayload.push({
+        releaseSiteName: trappingSite.releaseSiteName,
+        release_site_x_coord: trappingSite.releaseSiteXCoord,
+        release_site_y_coord: trappingSite.releaseSiteYCoord,
+      })
+    })
+    const createdTrapLocations = await knex<TrapLocations>(
+      'trapLocations'
+    ).insert(trappingSitesPayload, ['*'])
+
+    if (releaseSitesPayload) {
+      for (let i = 0; i < createdTrapLocations.length; i++) {
+        releaseSitesPayload[i].trapLocationsId = createdTrapLocations[i].id
+      }
+      await postReleaseSite(releaseSitesPayload)
+    }
+
+    return createdTrapLocations
+  } catch (error) {
+    throw error
+  }
+}
+
+export { getProgramTrapLocations, postTrapLocations, updateTrapLocations }
