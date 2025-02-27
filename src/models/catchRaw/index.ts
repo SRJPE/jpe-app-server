05cc40c21d3f3c053faa6cc5e5e0cdd17d3bb22b
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-02-27 10:44:54 -0600

    fixes qc put objects

--- src/models/catchRaw/index.ts
+++ src/models/catchRaw/index.ts
@@ -351,11 +351,18 @@ async function putCatchRaw(
     if (catchRawObject.createdExistingMarksResponse) {
       updatedExistingMarks = await Promise.all(
         catchRawObject.createdExistingMarksResponse.map(async existingMark => {
-          let id = existingMark.id
-          delete existingMark.id
-          await knex<ExistingMarksI>('existingMarks')
-            .where('id', id)
-            .update(existingMark, ['*'])
+          let id = existingMark?.id
+          delete existingMark?.id
+          if (id) {
+            const updatedExistingMark = await knex<ExistingMarksI>(
+              'existingMarks'
+            )
+              .where('id', id)
+              .update(existingMark, ['*'])
+            return updatedExistingMark[0]
+          } else {
+            return existingMark
+          }
         })
       )
     }
