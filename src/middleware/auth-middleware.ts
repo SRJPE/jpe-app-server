6628a7fefc9ff01c1679788742ad8096cb36df58
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-05-29 13:06:42 -0500

    adds isAuthorized logic

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import passport from 'passport'
+import { getPersonnelByAzureUid } from '../models/personnel'
+import { getPersonnelPrograms } from '../models/program'
 
 export async function isAuthenticated(
   req: Request,
@@ -19,20 +21,19 @@ export async function isAuthenticated(
         'oauth-bearer',
         { session: false },
         (err, payload, info) => {
-          if (err || !payload) {
+          if (err || !payload || !info) {
             console.error(err)
             return res
               .status(401)
               .send({ message: 'Token provided is not valid' })
           }
 
-          // res.locals = {
-          //   ...res.locals,
-          //   role: info['extension_Role'] || 'user',
-          //   email: info.emails[0],
-          //   uid: info.sub,
-          //   name: `${info.given_name} ${info.family_name}`,
-          // }
+          const azureUid = info?.sub
+          console.log('Azure UID:', azureUid)
+          res.locals = {
+            ...res.locals,
+            azureUid,
+          }
           return next()
         }
       )(req, res, next)
@@ -44,22 +45,49 @@ export async function isAuthenticated(
 }
 
 // TO DO: Refactor for our use case
-// export function isAuthorized(options: {
-//   hasRole?: any
-//   allowSameUser?: boolean
-// }) {
-//   return (req: Request, res: Response, next: Function) => {
-//     const { role, email, uid } = res.locals
+export function isAuthorized() {
+  return async (req: Request, res: Response, next: Function) => {
+    const { azureUid } = res.locals
 
-//     const { azureUid } = req.params
-//     if (options.allowSameUser && azureUid === uid) return next()
+    const tokenPersonnel = await getPersonnelByAzureUid(azureUid)
+    const { id, email } = tokenPersonnel
 
-//     if (!role) return res.status(403).send()
+    const tokenPrograms = await getPersonnelPrograms(id)
+    const tokenProgramIds = tokenPrograms.map(program => program.id)
 
-//     if (options.hasRole.includes(role)) return next()
+    if (req.params.azureUid && req.params.azureUid !== azureUid) {
+      console.log('Azure UID mismatch:', azureUid, req.params.azureUid)
+      return res
+        .status(403)
+        .send(`Access to the requested resource is forbidden for ${email}`)
+    }
 
-//     return res
-//       .status(403)
-//       .send(`Access to the requested resource is forbidden for ${email}`)
-//   }
-// }
+    if (
+      req.params.personnelId &&
+      tokenPersonnel.id !== Number(req.params.personnelId)
+    ) {
+      console.log('Personnel ID mismatch:', id, req.params.personnelId)
+      return res
+        .status(403)
+        .send(`Access to the requested resource is forbidden for ${email}`)
+    }
+
+    if (
+      req.params.programId &&
+      !tokenProgramIds.includes(Number(req.params.programId))
+    ) {
+      console.log('Program ID mismatch:', tokenProgramIds, req.params.programId)
+      return res
+        .status(403)
+        .send(`Access to the requested resource is forbidden for ${email}`)
+    }
+
+    // if (options.allowSameUser && azureUid === uid) return next()
+
+    // if (!role) return res.status(403).send()
+
+    // if (options.hasRole.includes(role)) return next()
+
+    return next()
+  }
+}
