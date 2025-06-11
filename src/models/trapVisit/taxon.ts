dc5ffa19f2161a050ce123ae488dac7510c8b033
wdwiii <wdwhitfield3@gmail.com>
2025-06-10 23:56:01 -0400

    refactor: update getTaxonWithAbbreviations to streamline data retrieval and remove unused code

--- src/models/trapVisit/taxon.ts
+++ src/models/trapVisit/taxon.ts
@@ -17,26 +17,40 @@ async function getTaxon(): Promise<Array<DropdownOption>> {
 //get taxon abbreviations
 async function getTaxonWithAbbreviations(): Promise<Array<any>> {
   try {
-    const taxonAbbreviations = await knex('taxonAbbreviation').select('*')
+    const taxonWithAbbreviations = await knex('programTaxonAbbreviation')
+      .join('program', 'program.id', 'programTaxonAbbreviation.programId')
+      .join(
+        'taxonAbbreviation',
+        'taxonAbbreviation.id',
+        'programTaxonAbbreviation.taxonAbbreviationId'
+      )
+      .join('taxon', 'taxon.code', 'taxonAbbreviation.taxonCode')
+      .select(
+        'programTaxonAbbreviation.*',
+        'taxon.commonname as commonname',
+        'taxonAbbreviation.*'
+      )
 
     const taxon = await knex('taxon').select('*')
 
-    const taxonAbbreviationMap = taxonAbbreviations.reduce((map, abbr) => {
-      if (!map[abbr.taxonCode]) {
-        map[abbr.taxonCode] = []
-      }
-      map[abbr.taxonCode].push(abbr.abbreviationCode)
-      return map
-    }, {} as Record<string, string[]>)
+    // const taxonAbbreviationMap = taxonAbbreviations.reduce((map, abbr) => {
+    //   if (!map[abbr.taxonCode]) {
+    //     map[abbr.taxonCode] = []
+    //   }
+    //   map[abbr.taxonCode].push(abbr.abbreviationCode)
+    //   return map
+    // }, {} as Record<string, string[]>)
 
-    const taxonWithAbbreviations = taxon.map(currentTaxonRecord => {
-      currentTaxonRecord.taxonAbbreviations =
-        taxonAbbreviationMap[currentTaxonRecord.code] || []
-      return currentTaxonRecord
-    })
+    // const taxonWithAbbreviations = taxon.map(currentTaxonRecord => {
+    //   currentTaxonRecord.taxonAbbreviations =
+    //     taxonAbbreviationMap[currentTaxonRecord.code] || []
+    //   return currentTaxonRecord
+    // })
 
     return taxonWithAbbreviations
   } catch (error) {
+    console.log('🚀 ~ taxon.ts:54 ~ getTaxonWithAbbreviations ~ error:', error)
+
     throw error
   }
 }
