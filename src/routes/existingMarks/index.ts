c03ca32ddf5dc74be5b1099709246cebc9b672bf
Jordan Hoang <jordan.n.hoang@gmail.com>
2026-06-29 15:42:58 -0500

    implmenets qc functionlaity for gneetics and marks

--- src/routes/existingMarks/index.ts
+++ src/routes/existingMarks/index.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express'
-import { getProgramExistingMarks } from '../../models/catchRaw/existingMarks'
+import {
+  getProgramExistingMarks,
+  putExistingMark,
+} from '../../models/catchRaw/existingMarks'
 
 const existingMarksRouter = Router({ mergeParams: true })
 
 export default (mainRouter: Router) => {
   mainRouter.use('/existing-marks', existingMarksRouter)
+
+  existingMarksRouter.put('/:existingMarkId', async (req, res) => {
+    try {
+      const { existingMarkId } = req.params
+      const updated = await putExistingMark(existingMarkId, req.body)
+      res.status(200).send(updated)
+    } catch (error) {
+      console.error(error)
+      res.status(400).send(error)
+    }
+  })
 
   existingMarksRouter.get('/program/:programId', async (req, res) => {
     try {
